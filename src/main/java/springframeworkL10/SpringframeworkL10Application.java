c05f4c045ab6f918f55b6845da445b7f36d9d236
Torok Roland <torok.roland1998@gmail.com>
2023-08-01 21:33:29 +0300

    Starting the lesson 10

---
+++ src/main/java/springframeworkL10/SpringframeworkL10Application.java
@@ -0,0 +1,13 @@
+package springframeworkL10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkL10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkL10Application.class, args);
+    }
+
+}
---
+++ src/test/java/springframeworkL10/SpringframeworkL10ApplicationTests.java
@@ -0,0 +1,13 @@
+package springframeworkL10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkL10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
